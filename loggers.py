94fc9e46d0de621aa861a603a28661c21c846621
スタークギャング <StarkGangZ@gmail.com>
2022-03-12 20:15:05 +0530

    .

--- loggers.py
+++ loggers.py
@@ -2,7 +2,7 @@ import logging
 
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 logging.basicConfig(
-            level=logging.DEBUG,
+            level=logging.INFO,
             datefmt="[%d/%m/%Y %H:%M:%S]",
             format="%(asctime)s - [STREAMERUB] >> %(levelname)s << %(message)s",
             handlers=[logging.FileHandler("streamerub.log"), logging.StreamHandler()],
